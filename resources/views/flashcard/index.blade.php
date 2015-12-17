e65ff794fae930c1ccaaa5a3bce97f02c83c3a20
Aran Gilmore <aran@arangilmore.com>
2015-12-16 19:41:12 -0500

    Modified form for Flashcard

--- resources/views/flashcard/index.blade.php
+++ resources/views/flashcard/index.blade.php
@@ -20,50 +20,59 @@ such as a page specific stylesheets.
     <h1>Show Flashcards</h1>
 
 	<form method="POST" action="/flashcard">
+    	<input type="hidden" name="_token" value="{{ csrf_token() }}">
 	    <fieldset id="flashdata">
-	    	<input type="hidden" name="_token" value="{{ csrf_token() }}">
-	    	<legend>Flashcard Data</legend>
-	    	<label for="item">Word or phrase</label>
-	    	<input type="text" id="item" autofocus name="item" placeholder="e.g. Input Types">
+	    	<h3>Flashcard Data</h3>
+	    	<label for="card_entry">Word or phrase</label>
+	    	<input type="text" id="card_entry" name="card_entry" placeholder="Input Types">
 	    	<br>
-	    	<label for="definition">Personal written definition</label>
-	    	<input type="text" id="definition" name="definition" placeholder="e.g. Describes specified type of input in an HTML form">
+	    	<label for="definition_entry">Personal written definition</label>
+	    	<input type="text" id="definition_entry" name="definition_entry" placeholder="Describes specified type of input in an HTML form">
 	    	<br>
-	    	<label for="relation">Personal relation or use of said term or action</label>
-	    	<input type="text" id="relation" name="relation" placeholder="e.g. In a form requiring a phone number, you can define the type as 'tel' to specify it as a telephone number">
+	    	<label for="relation_entry">Personal relation or use of said term or action</label>
+	    	<input type="text" id="relation_entry" name="relation_entry" placeholder="In a form requiring a phone number, you can define the type as 'tel' to specify it as a telephone number"></textarea>
 	    	<br>
-	    	<label for="itemurl">URL of page describing the term and its use definition</label>
-	    	<input type="url" id="itemurl" name="itemurl" placeholder="e.g. http://www.w3schools.com/html/html_form_input_types.asp">
-	    </fieldset>
-	    <fieldset>
-	    	<legend>School and class data (for later reference)</legend>
-	    	<label for="school">School name (e.g. Harvard Extension School)</label>
-	    	<select id="school">
-	    		<option checked>Select a school from the list:</option>
-	    		<option value="HES">Harvard Extension School</option>
-	    		<option value="SEAS">School for Engineering and Applied Sciences</option>
-	    		<option value="GSE">Graduate Schol of Education</option>
-		    </select>
-		    <br>
+	    	<label for="card_entry_url">URL of page describing the term and its use definition</label>
+	    	<input type="url" id="card_entry_url" name="card_entry_url" placeholder="http://www.w3schools.com/html/html_form_input_types.asp">
+			<br>
+	    	<div class="ui-field-contain">
+		    	<h4>School and class data (for later reference)</h4>
+		    	<label for="school">School name (e.g. Harvard Extension School)</label>
+		    	<select name="school" id="school">
+		    		<option>Select a school from the list:</option>
+		    		<option value="HES">Harvard Extension School</option>
+		    		<option value="SEAS">School for Engineering and Applied Sciences</option>
+		    		<option value="GSE">Graduate Schol of Education</option>
+			    </select>
+		    </div>	
 		    <label for="department">Class department (eg CSCI)</label>
 	    	<input type="text" id="department" autocomplete="on" name="department">
 	    	<br>
-	    	<label for="area">Class Area (eg. E)</label>
-	    	<input type="text" id="area" autocomplete="on" name="area">
+	    	<label for="area" class="select">Class Area (eg. E)</label>
+	    	<select id="area" name="area" data-mini="true" data-inline="true">
+	    		<option value="a">A</option>
+	    		<option value="b">B</option>
+		    	<option value="c">C</option>
+		    	<option value="d">D</option>
+		    	<option value="e">E</option>
+	    	</select>
 	    	<br>
 	    	<label for="classnumber">Class number (e.g. 20)</label>
 	    	<input type="text" id="classnumber" name="classnumber">
-	    	<br>
-	    	<label for="semester">Semester</label>
-	    	<input type="text" id="semester" autocomplete="on" name="semester">
-	    	<br>
+	    <div>
+		    	<label for="semester">Semester</label>
+		    	<select type="text" id="semester" autocomplete="on" name="semester">
+		    		<option value="fall">Fall</option>
+		    		<option value="winter">Winter</option>
+			    	<option value="spring">Spring</option>
+			    	<option value="summer">Summer</option>
+				</select>
+	    </div>
 	    	<label for="year">Year</label>
 	    	<input type="number" id="year" name="year" max="9999" min="1900" value="2015">
-	    </fieldset>
-	    <fieldset>
-	    	<legend>Submit</legend>
-	    	<label for="submit">Submit</label>
-	    	<input type="submit" id="submit" name="submit" value="Submit Flashcard">
+	    	<label class="ui-hidden-accessible" for="submit"></label>
+			<br>
+	    	<button id="submit" name="submit" value="Submit Flashcard" class="ui-btn ui-corner-all">Submit</button>
 	    </fieldset>
 	</form>
 
